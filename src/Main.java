03662075ac0c7fffb8461fb9d50eb3384f304202
ALEKSEY <hattaeck@gmail.com>
2022-02-08 20:26:05 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
-
     public static void main(String[] args) {
-        int currentaccbalance = 2_000_000_000;
-        int transfersumm = 500_000_000;
-        int totalsumm = transfersumm + currentaccbalance;
-        System.out.println(totalsumm);
-
+        double regularBonus = 0.3;
+        double specialBonus = 0.6;
+        double totalBonus = regularBonus + specialBonus;
+        System.out.println(totalBonus);
     }
 }
